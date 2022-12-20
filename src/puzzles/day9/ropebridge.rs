d288c5f44088c1d2fe412b1ee3f466f362cbd883
postfen <26173007+postfen@users.noreply.github.com>
2022-12-19 23:32:41 -0700

    Day 9 refactoring

--- src/puzzles/day9/ropebridge.rs
+++ src/puzzles/day9/ropebridge.rs
@@ -1,14 +1,14 @@
 use std::collections::HashSet;
+/*
+                             --- Day 9: Rope Bridge ---
+                              A. 6406
+                              B. 2643
+*/
 
 pub fn print_solution() {
     let s = include_str!("input");
-    let s1 = q1(s);
-    let s2 = q2(s);
-    println!(
-        "--- Day 9: Rope Bridge ---\n\
-         A. {s1}\n\
-         B. {s2}\n"
-    );
+    let (s1, s2) = solve(s);
+    println!("--- Day 9: Rope Bridge ---\nA. {s1}\nB. {s2}\n");
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Copy)]
@@ -17,73 +17,65 @@ struct Coord {
     y: isize,
 }
 
-fn move_in_direction(knot: &mut Coord, dir_char: &str) {
-    match dir_char {
-        "U" => knot.y -= 1,
-        "D" => knot.y += 1,
-        "L" => knot.x -= 1,
-        "R" => knot.x += 1,
-        &_ => panic!("Can't move in that direction"),
+impl Coord {
+    fn not_touching(&self, other: &Coord) -> bool {
+        self.x.abs_diff(other.x) > 1 || self.y.abs_diff(other.y) > 1
     }
-}
 
-fn q1(s: &str) -> i32 {
-    let mut seen: HashSet<_> = HashSet::new();
-    let start_pos = Coord { x: 0, y: 0 };
-    let mut head = start_pos;
-    let mut tail = start_pos;
-    seen.insert(tail);
-    for line in s.lines() {
-        let (direction, amount) = line.split_once(' ').unwrap();
-        let amount = amount.parse::<isize>().unwrap();
-        for _ in 0..amount {
-            move_in_direction(&mut head, direction);
-            if not_touching(&head, &tail) {
-                tail.x += (head.x - tail.x).signum();
-                tail.y += (head.y - tail.y).signum();
-                seen.insert(tail);
-            }
+    fn move_by_coords(&mut self, dir_char: &str) {
+        match dir_char {
+            "U" => self.y -= 1,
+            "D" => self.y += 1,
+            "L" => self.x -= 1,
+            "R" => self.x += 1,
+            _ => panic!("Can't move in that direction"),
         }
     }
-    seen.len().try_into().unwrap()
-}
-fn not_touching(head: &Coord, tail: &Coord) -> bool {
-    head.x.abs_diff(tail.x) > 1 || head.y.abs_diff(tail.y) > 1
 }
 
-fn q2(s: &str) -> i32 {
-    let mut seen: HashSet<_> = HashSet::new();
+fn solve(input: &str) -> (i32, i32) {
+    let mut s1: HashSet<_> = HashSet::new();
+    let mut s2: HashSet<_> = HashSet::new();
     let start_pos = Coord { x: 0, y: 0 };
     let mut rope = vec![start_pos; 10];
-    seen.insert(start_pos);
-    for line in s.lines() {
+    let mut head = start_pos;
+    let mut tail = start_pos;
+    s1.insert(tail);
+    s2.insert(start_pos);
+    for line in input.lines() {
         let (direction, amount) = line.split_once(' ').unwrap();
         let amount = amount.parse::<isize>().unwrap();
         for _ in 0..amount {
-            move_in_direction(&mut rope[0], direction);
-            for head in 0..rope.len() - 1 {
-                let tail = head + 1;
-                if not_touching(&rope[head], &rope[tail]) {
-                    rope[tail].x += (rope[head].x - rope[tail].x).signum();
-                    rope[tail].y += (rope[head].y - rope[tail].y).signum();
-                    if tail == rope.len() - 1 {
-                        seen.insert(rope[rope.len()- 1]);
+            head.move_by_coords(direction);
+            rope[0].move_by_coords(direction);
+            if head.not_touching(&tail) {
+                tail.x += (head.x - tail.x).signum();
+                tail.y += (head.y - tail.y).signum();
+                s1.insert(tail);
+            }
+            for first in 0..rope.len() - 1 {
+                let second = first + 1;
+                if rope[first].not_touching(&rope[second]) {
+                    rope[second].x += (rope[first].x - rope[second].x).signum();
+                    rope[second].y += (rope[first].y - rope[second].y).signum();
+                    if second == rope.len() - 1 {
+                        s2.insert(rope[rope.len() - 1]);
                     }
                 }
             }
         }
     }
-    seen.len().try_into().unwrap()
+    (s1.len().try_into().unwrap(), s2.len().try_into().unwrap())
 }
 
 #[cfg(test)]
 #[test]
 fn test_dayn_a() {
     let s = include_str!("test_input");
-    assert_eq!(q1(s), 13);
+    assert_eq!(solve(s).0, 13);
 }
 #[test]
 fn test_dayn_b() {
     let s = include_str!("test_input2");
-    assert_eq!(q2(s), 36);
+    assert_eq!(solve(s).1, 36);
 }
